176d2afb066dffd7cb5e46f0a9ca2773414ce8ba
acaty64 <ana.arashiro@gmail.com>
2018-01-28 18:29:12 +0000

    first commit

---
+++ resources/views/test.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+@section('title','Vista de Prueba')
+@section('content')
+    <div class="container">
+
+		<Example-component></Example-component>
+
+	</div>
+@endsection
+
+@section('view','help.blade.php')
